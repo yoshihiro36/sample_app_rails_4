23d9e084606eb88a439ed608089805226378241b
yoshihiro <yosihiro.mizusawa@gmail.com>
2015-04-25 13:58:08 +0900

    Finish user signup

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 SampleApp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -33,4 +34,7 @@ SampleApp::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  # bcrypt'のコスト関数を下げることでテストの速度を向上させる。
+  ActiveModel::SecurePassword.min_cost = true
 end
